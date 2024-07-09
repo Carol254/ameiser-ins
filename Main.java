9ae8fcee3706b1f3eb93fd64e2d5b9e772840eaa
Caroline <wanguic49@gmail.com>
2024-07-09 15:25:03 +0300

    few changes made

--- Main.java
+++ Main.java
@@ -3,8 +3,19 @@ public class Main {
         String name = "John";
         int age = 23;
         boolean myBool = false;
+        char myLetter = 'l';
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName  +  lastName;
+        int x = 5;
+        int y = 6;
+
         System.out.println(myBool);
         System.out.println(name);
         System.out.println(age);
+        System.out.println(myLetter);
+        System.out.println("Hello " + name);
+        System.out.println(fullName);
+        System.out.println(x + y);
     }
   }
